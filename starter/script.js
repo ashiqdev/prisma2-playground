3386aeccf31898c303664ce6052e51cc290e4f04
Ashiqur Rahman <ashiquejava@gmail.com>
2020-05-22 11:14:52 +0600

    feat: write something into db

--- starter/script.js
+++ starter/script.js
@@ -4,6 +4,16 @@ const prisma = new PrismaClient();
 
 // A `main` function so that you can use async/await
 async function main() {
+  const post = await prisma.post.create({
+    data: {
+      title: "Prisma makes database easy",
+      author: {
+        connect: { email: "maria@prisma.io" },
+      },
+    },
+  });
+
+  console.log({ post });
   const allUsers = await prisma.user.findMany({ include: { posts: true } });
   // use console.dir to print nested objects
   console.dir(allUsers, { depth: null });
