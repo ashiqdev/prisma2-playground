d2b782c0e59d7789825dc32a87e553df372f526c
Ashiqur Rahman <ashiquejava@gmail.com>
2020-05-22 11:28:51 +0600

    feat: completed crud functionality

--- starter/script.js
+++ starter/script.js
@@ -4,19 +4,39 @@ const prisma = new PrismaClient();
 
 // A `main` function so that you can use async/await
 async function main() {
-  const post = await prisma.post.create({
-    data: {
-      title: "Prisma makes database easy",
-      author: {
-        connect: { email: "maria@prisma.io" },
-      },
-    },
+  // create a post
+  // const post = await prisma.post.create({
+  //   data: {
+  //     title: "Prisma makes database easy",
+  //     author: {
+  //       connect: { email: "maria@prisma.io" },
+  //     },
+  //   },
+  // });
+
+  // print the created post in console
+  // console.log({ post });
+
+  // update the post
+  const post = await prisma.post.update({
+    where: { id: 2 },
+    data: { published: true, title: "Prisma makes database difficult" },
   });
 
-  console.log({ post });
+  // delete the post
+  // const post = await prisma.post.delete({
+  //   where: { id: 3 },
+  // });
+
+  // fetch all users with their posts
   const allUsers = await prisma.user.findMany({ include: { posts: true } });
+
   // use console.dir to print nested objects
   console.dir(allUsers, { depth: null });
+
+  // fetch all posts
+  const allPosts = await prisma.post.findMany();
+  console.log({ allPosts });
 }
 
 main()
